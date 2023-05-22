9140b6875ea2bda2bbd3170519d4468e994ddf01
Laís Carinhanha <104446657+laiscarinhanha@users.noreply.github.com>
2023-05-21 23:33:32 -0300

    Add files via upload

---
+++ recursosnavegador.js
@@ -0,0 +1,21 @@
+let nome = "Laís"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+//window.alert(nome)
+
+//nome = window.prompt("Digite seu Nome") 
+
+//o prompt permite que a gente salve esses valores para trabalhar dentro do código
+//conseguimor perguntar algo ao usuário para inserir e manipular essa informação no nosso código
+
+//console.log(nome)
+
+let confirmação = window.confirm("Voce confirma essa ação?")
+console.log(confirmação)
+
+//se clicar em ok parece 'true' se clicar em cancelar aparece 'false'
+
+
